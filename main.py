b6281f5d036990834a5596c13a8096cf5cf26663
AitazazGilani <47329791+AitazazGilani@users.noreply.github.com>
2021-08-30 02:48:42 -0600

    temporarily created a command for generating a random wallet

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import discord
 from web3 import Web3
+import setup as S
 client = discord.Client()
 
 url = "https://rpc-mainnet.maticvigil.com/v1/b20459a39192d4978e017db439b3a37d314059ae"
 web3 = Web3(Web3.HTTPProvider(url))
-test = web3.isConnected()
 
 @client.event
 async def on_ready():
@@ -19,9 +19,13 @@ async def on_message(message):
         await message.channel.send('Pong')
 
     if message.content.startswith('$web3 polygon status'):
+        test = web3.isConnected()
         if test == True:
             await message.channel.send('Live!')
         else:
             await message.channel.send('Connection offline')
 
+    if message.content.startswith('$generate wallet'):
+        account1 = S.create_account()
+        await message.channel.send(str(account1.address))
 client.run('ODU1MjI1MDUzMDIwNTUzMjM2.YMvYZQ.VaBz5KJ2bn4-ignx0ilGJgdK17k')
