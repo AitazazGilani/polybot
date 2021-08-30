1594c6d6dfd38245f044da3c42e94de3cc25183d
AitazazGilani <47329791+AitazazGilani@users.noreply.github.com>
2021-08-30 02:39:57 -0600

    created a file for containing api functions. Created a function for creating an eth wallet

---
+++ api-testnet.py
@@ -0,0 +1,13 @@
+from web3 import Web3, Account
+import secrets
+
+def create_account():
+    '''
+    create an ethereum wallet
+    :returns: an object class containing info about the account
+    such as wallet address, private key, etc
+    '''
+    private_key = "0x" + secrets.token_hex(32)
+    acct = Account.from_key(private_key)
+    return acct
+
