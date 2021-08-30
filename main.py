3de6c2f43422c4e33ee605dae9eef3b207af0562
AitazazGilani <47329791+AitazazGilani@users.noreply.github.com>
2021-08-30 02:02:17 -0600

    added a command to test connection to matic

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import discord
-
+from web3 import Web3
 client = discord.Client()
+
+url = "https://rpc-mainnet.maticvigil.com/v1/b20459a39192d4978e017db439b3a37d314059ae"
+web3 = Web3(Web3.HTTPProvider(url))
+test = web3.isConnected()
 
 @client.event
 async def on_ready():
@@ -14,7 +18,10 @@ async def on_message(message):
     if message.content.startswith('$ping'):
         await message.channel.send('Pong')
 
-    if message.content.startswith('$generate wallet'):
-        await message.channel.send('Pong')
+    if message.content.startswith('$web3 polygon status'):
+        if test == True:
+            await message.channel.send('Live!')
+        else:
+            await message.channel.send('Connection offline')
 
 client.run('ODU1MjI1MDUzMDIwNTUzMjM2.YMvYZQ.VaBz5KJ2bn4-ignx0ilGJgdK17k')
