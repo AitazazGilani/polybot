8538ab90d544af19bf4afaf49c14b4d59440c3dc
AitazazGilani <47329791+AitazazGilani@users.noreply.github.com>
2021-09-05 01:10:53 -0600

    added database support for get and generate commands, added 2 new temporary commands

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from discord.ext import commands
 import setup as S
 import network as n
 client = commands.Bot(command_prefix='!')
-
+#S.create_db()
 
 @client.event
 async def on_ready():
@@ -28,31 +28,38 @@ async def web3(ctx,arg):
 @client.command()
 async def generate(ctx,arg):
     if arg == 'wallet':
-        acc = S.create_account() #need to make a data base for the user here
-        await ctx.send(acc.address)
+        acc = S.create_account()
+        if S.fetch_user(ctx.author.id) == []:
+            S.add_user(ctx.author.id,acc.address,acc.privateKey)
+            await ctx.send("your address:",acc.address)
+        else:
+            await ctx.send("your wallet has already been created")
 
 @client.command()
 async def get(ctx,*args):
     if args[0] == 'balance':
-        user_adr = str(args[1]) #holds wallet address
+        user = S.fetch_user(ctx.author.id) #holds wallet address
         # check if wallet is correct
-        check = n.check_addr(user_adr)
-        if check == True:
-            balance = n.get_bal(user_adr)
-            await ctx.send(str(balance) + " ETH")
+        check = user
+        if check == []:
+            await ctx.send("You do not have any active wallets")
         else:
-            await ctx.send("address not found")
-#!send value addr1 privkey(addr1) addr2
+            balance = n.get_bal(user[1])
+            await ctx.send('{} {}'.format(balance,'COOM'))
+
+
+#!send value ticker addr1 privkey(addr1) addr2
+#!send value ticker @username
 @client.command()
 async def send(ctx,*args):
     value = args[0]
     try: #check if given value is a number
         value = float(value)
     except:
-        await ctx.send("invalid amount of eth to be transacted")
-    addr_1 = args[1]
-    priv = args[2]
-    addr_2 = args[3]
+        await ctx.send("invalid amount of COOM to be transacted")
+    addr_1 = args[2]
+    priv = args[3]
+    addr_2 = args[4]
 
     if (n.check_addr(addr_1) == False) or (n.check_addr(addr_2) == False) :
         await ctx.send("invalid address given")
@@ -73,10 +80,15 @@ async def send(ctx,*args):
 
     await ctx.send("Transactions successful\n Contract Address: " + tx_confirm)
 
+@client.command()
+async def user(ctx):
+    await ctx.send(ctx.author.id)
 
-
-
-
-
+@client.command(name='get-uid')
+async def get_uid(ctx, *, user: discord.User=None):
+    if user != None:
+        await ctx.send(f'{user.id}')
+    else:
+        await ctx.send('user not found')
 
 client.run('ODU1MjI1MDUzMDIwNTUzMjM2.YMvYZQ.VaBz5KJ2bn4-ignx0ilGJgdK17k')
